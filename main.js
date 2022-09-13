2b1f07b0abeb406d938484d1d670d9af05f83946
Venia <vsollery@gmail.com>
2022-09-13 13:54:29 +0700

    draw one point on data

--- main.js
+++ main.js
@@ -2,19 +2,27 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
   
+    //VERTEX SHADER
     var vertexShaderCode = `
-    void main() {
-        
-    }
+      void main() {
+        gl_PointSize = 100.0;
+        gl_Position = vec4(-1.0, 1.0, 0.0, 1.0);
+        //gl_Position is the Final destination for storing
+        //positional data for the rendered vertex
+      }
     `;
   
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
   
+    //FRAGMENT SHADER --> manage color data
     var fragmentShaderCode = `
     void main() {
-  
+      gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+      //gl_FragColor is the final destinatio for storing
+      //color data for rendered fragment
+      
     }
     `;
   
@@ -30,5 +38,7 @@ function main() {
   
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINT, 0, 1); //mode -> primitive assembly
   }
   
