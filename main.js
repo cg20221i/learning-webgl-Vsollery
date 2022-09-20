7a7a38da506b8c71c0733e74ea912ae7e56d246b
Venia <vsollery@gmail.com>
2022-09-20 14:11:24 +0700

    rotate

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
-
+    
     //A( 0.5, 0.5)
     //B( 0.0, 0.0)
     //C(-0.5, 0.5)
@@ -9,7 +9,8 @@ function main() {
     var vertices =[
         0.5, 0.5, 
         0.0, 0.0, 
-        -0.5, 0.5
+        -0.5, 0.5,
+        0.0, 1.0
     ];
 
     //Create a linked-list for storing vertices data in GPU
@@ -20,9 +21,13 @@ function main() {
     //VERTEX SHADER
     var vertexShaderCode = `
       attribute vec2 aPosition;
+      uniform float uTheta;
       void main() {
         gl_PointSize = 15.0;
-        gl_Position = vec4(aPosition, 0.0, 1.0);
+        vec2 position;  vec2(aPosition);
+        position.x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
+        position.y = sin(uTheta) * aPosition.y + cos(uTheta) * aPosition.x;
+        gl_Position = vec4(position, 0.0, 1.0);
         //gl_Position is the Final destination for storing
         //positional data for the rendered vertex
       }
@@ -53,6 +58,11 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
+    //All the quilifiers needed by shaders
+    var uTheta = gl.getUniformLocation(shaderProgram, "uTheta")
+
+    var theta = 0.0;
+
     //Teach computer how to collect the positional values for ARRAY_BUFFEr
     //for each vertex being proccesed
 
@@ -60,9 +70,14 @@ function main() {
     gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0,0 )
     gl.enableVertexAttribArray(aPosition);
   
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    const render = () => {
+      gl.clearColor(1.0, 0.75, 0.79, 1.0);
+      theta += 0.01;
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      gl.uniform1f(uTheta,theta);
 
-    gl.drawArrays(gl.POINT, 0, 3); //mode -> primitive assembly
+      gl.drawArrays(gl.TRIANGLE_FAN, 0, 4); //mode -> primitive assembly
+    }
+    setInterval(render,1000/60);
   }
   
